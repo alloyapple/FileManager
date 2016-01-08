bdbb354b02acb96b2879d58748315ce4d56bb090
alloy.apple <alloy.apple@gmail.com>
2016-01-08 14:12:00 +0800

    delete unused code

--- Sources/FileManager/GFileManager.swift
+++ Sources/FileManager/GFileManager.swift
@@ -64,21 +64,6 @@ public class GFileManager {
   }
 
   public func HomeDir() -> String {
-    /*
-    let uid = getuid()
-    let homeDirStruct = getpwuid(uid)
-
-    let HomeDirString = withUnsafePointer(&homeDirStruct.memory.pw_dir, { (ptr) -> String? in
-      let int8Ptr = unsafeBitCast(ptr, UnsafePointer<Int8>.self)
-      return String.fromCString(int8Ptr)
-    })
-
-    if let HomeDirString = HomeDirString {
-      return HomeDirString
-    } else {
-      return ""
-    }
-    */
 
     let home = getenv("HOME")
     let homeString = String.fromCString(home)
@@ -97,9 +82,7 @@ public class GFileManager {
 
   public func CreateFile(filePath: String) -> Bool {
     let fileId = creat(filePath, S_IRWXG)
-
     guard fileId != -1 else { return false }
-    //fchmod(fileId, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
     close(fileId)
     return true
   }
