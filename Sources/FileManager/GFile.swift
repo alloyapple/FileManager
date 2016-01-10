3515d2fb56226f603e8c60e3f03437487cacddfb
ftzheng <alloy.apple@163.com>
2016-01-11 07:43:39 +0800

    Add functions to GFile

--- Sources/FileManager/GFile.swift
+++ Sources/FileManager/GFile.swift
@@ -92,6 +92,64 @@ public class GFile {
     }
   }
 
+  public func CanExecute() -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+
+    return fileManager.CanExecute(self.filePath)
+  }
+
+
+  public func SetExecute(canexe: Bool) -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+
+    return fileManager.SetExecute(self.filePath, canexe: canexe)
+  }
+
+
+  public func CanRead(filePath: String) -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+    return fileManager.CanRead(self.filePath)
+  }
+
+
+  public func SetRead(canexe: Bool) -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+    return fileManager.SetRead(self.filePath, canexe: canexe)
+
+  }
+
+  public func CanWrite() -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+    return fileManager.CanWrite(self.filePath)
+  }
+
+
+  public func SetWrite(canexe: Bool) -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+    return fileManager.SetWrite(self.filePath, canexe: canexe)
+  }
+
+  public func IsDirectory() -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+    return fileManager.IsDirectory(self.filePath)
+  }
+
+  public func IsFile() -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+    return fileManager.IsFile(self.filePath)
+  }
+
+
+  public func IsHidden() -> Bool {
+    let fileManager = GFileManager.defaultFileManager
+    return fileManager.IsHidden(self.filePath)
+  }
+
+  public func GetFileSize() -> FileSize? {
+    let fileManager = GFileManager.defaultFileManager
+    return fileManager.GetFileSize(self.filePath)
+  }
+
   deinit {
     if fp != nil {
       fclose(fp)
