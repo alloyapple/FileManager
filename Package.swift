a0d7a5e854597a46e11375cd19998afa48b3dce1
alloy.apple <alloy.apple@gmail.com>
2016-01-13 11:39:11 +0800

    Add depences lib

--- Package.swift
+++ Package.swift
@@ -5,5 +5,10 @@ let package = Package(
   targets: [
     Target(name: "UnitTests", dependencies: [.Target(name: "FileManager")]),
     Target(name: "FileManager")
+  ],
+  dependencies: [
+  .Package(url: "https://github.com/alloyapple/swiftwrappingc.git", majorVersion: 0)
+  //.Package(url: "https://git.oschina.net/GoldTeam/swiftwrappingc.git", majorVersion: 1, minor: 0)
+  //  .Package(url: "../swiftdev/swiftc/", majorVersion: 0, minor: 2)
   ]
 )
